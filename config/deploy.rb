7027fbba408470c4e2b7ddb6e5138d165f3da2be
a1153tm <a1153tm@aiit.ac.jp>
2012-02-04 19:33:59 +0900

    add purblic/images

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, "locials"
+set :deploy_to, "/home/mhp/locials"
+
+set :repository,  "git@github.com:a1153tm/LocialS"
+set :scm, :git
+
+#set :user, :deployer  
+
+set :use_sudo, false
+
+role :web, "mhp@avenueq.tachibanakikaku.com"
+role :app, "mhp@avenueq.tachibanakikaku.com"
+#role :db,  "localhost", :primary => true # This is where Rails migrations will run
+#role :db,  "localhost"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
